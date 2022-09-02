f7302988bbd14e41b43b4ee6fabc21c987372bc8
Jen-Chieh Shen <jcs090218@gmail.com>
2022-09-03 01:15:30 +0800

    Add project

---
+++ Assets/_Project/Scripts/RunnerCtrl.cs
@@ -0,0 +1,31 @@
+/**
+ * $File: RunnerCtrl.cs $
+ * $Date: 2022-09-03 01:10:51 $
+ * $Revision: $
+ * $Creator: Jen-Chieh Shen $
+ * $Notice: See LICENSE.txt for modification and distribution information
+ *                   Copyright © 2022 by Shen, Jen-Chieh $
+ */
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+public class RunnerCtrl : MonoBehaviour
+{
+    /* Variables */
+
+    private CharacterController mCharacterController = null;
+
+    /* Setter & Getter */
+
+    /* Functions */
+
+    private void Awake()
+    {
+        this.mCharacterController = this.GetComponent<CharacterController>();
+    }
+
+    private void Update()
+    {
+        
+    }
+}
