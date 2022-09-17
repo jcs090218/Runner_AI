0369a3abd1d702906a1278346e48b810a9169665
Jen-Chieh Shen <jcs090218@gmail.com>
2022-09-17 20:41:19 +0800

    Start NN module

---
+++ Assets/_Project/Scripts/NeuralNetwork/Neuron.cs
@@ -0,0 +1,24 @@
+/**
+ * $File: Neuron.cs $
+ * $Date: 2022-09-17 17:20:17 $
+ * $Revision: $
+ * $Creator: Jen-Chieh Shen $
+ * $Notice: See LICENSE.txt for modification and distribution information
+ *                   Copyright © 2022 by Shen, Jen-Chieh $
+ */
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 
+/// </summary>
+public class Neuron : MonoBehaviour
+{
+    /* Variables */
+
+    /* Setter & Getter */
+
+    /* Functions */
+
+}
