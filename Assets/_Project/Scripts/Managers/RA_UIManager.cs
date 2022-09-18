5c5e42be82c946ab98a2511df19b8b921c3ab014
Jen-Chieh Shen <jcs090218@gmail.com>
2022-09-18 17:38:19 +0800

    add auto revive

--- Assets/_Project/Scripts/Managers/RA_UIManager.cs
+++ Assets/_Project/Scripts/Managers/RA_UIManager.cs
@@ -7,12 +7,34 @@
  *                   Copyright © 2022 by Shen, Jen-Chieh $
  */
 using UnityEngine;
+using UnityEngine.UI;
+using JCSUnity;
 
-public class RA_UIManager : MonoBehaviour
+public class RA_UIManager : JCS_Manager<RA_UIManager>
 {
     /* Variables */
+
+    [Header("** Runtime Variables (RA_UIManager) **")]
+
+    [Tooltip("Revive runners when possible.")]
+    [SerializeField]
+    private Toggle mToggle_AutoRevive = null;
 
     /* Setter & Getter */
 
     /* Functions */
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    private void Start()
+    {
+        var appm = RA_AppManager.instance;
+
+        mToggle_AutoRevive.onValueChanged.AddListener(delegate {
+            appm.autoRevive = mToggle_AutoRevive.isOn;
+        });
+    }
 }
